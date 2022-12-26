2f402331b04cbd86352b0be337782d77b18c86cd
Leonardo Dezan <leo.dezan@gmail.com>
2022-12-26 20:59:54 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,22 +1,6 @@
 <?php
 
-// $idade = 40;
 
-// if ($idade < 10){
-//     echo "primeira idade";
-// }elseif( $idade <= 40){
-//     echo "Segunda idade";
-// }else{
-//     echo "terceira idade";
-// }
-
-
-$x = 100;
-
-do {
-    echo "o valor é: $x <br/>";
-    $x++;
-} while ($x <= 20);
 
 
 ?>
