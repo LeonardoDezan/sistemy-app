00de2b97ee2f280c27da62cde82545f23702330c
Leonardo Dezan <leo.dezan@gmail.com>
2022-12-26 20:15:11 -0300

    teste

--- index.php
+++ index.php
@@ -1,9 +1,22 @@
 <?php
-//CONSTANTES
-define("nome", "leonardo");
 
-echo nome;
+// $idade = 40;
 
+// if ($idade < 10){
+//     echo "primeira idade";
+// }elseif( $idade <= 40){
+//     echo "Segunda idade";
+// }else{
+//     echo "terceira idade";
+// }
+
+
+$x = 100;
+
+do {
+    echo "o valor é: $x <br/>";
+    $x++;
+} while ($x <= 20);
 
 
 ?>
