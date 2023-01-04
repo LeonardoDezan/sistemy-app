e2bb192aaf5bbab8535e3036c9219d3d8771c75d
Leonardo Dezan <leo.dezan@gmail.com>
2023-01-04 20:05:41 -0300

    Pesquisa no banco

--- cadastro-clientes.php
+++ cadastro-clientes.php
@@ -1,5 +1,13 @@
 <!-- BACK END DA PAGINA cadastro-clientes.html -->
-
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pesquisar</title>
+</head>
+</html>
 
 <?php
 include_once "config.php";
@@ -27,6 +35,19 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
         echo "deu erro" . $sql . "<br/>" . mysqli_error($conexao);
    }
     mysqli_close($conexao);
+
+}
+
+
+if (isset($_GET['pesquisar'])){
+    $pesquisar = $_GET['pesquisar'];
+    $resultado_pesquisar = "SELECT * FROM tb_clientes WHERE razao_social like '%$pesquisar%'";
+    $resultado = mysqli_query($conexao, $resultado_pesquisar);
+    while ($linha = mysqli_fetch_array($resultado)){
+        echo $razao_social_busca = $linha['razao_social'];
+        echo "<br/>";
+    }
+
 
 }
 
