f3e106e8d696d1af19881c61650daf85a08c9b53
Leonardo Dezan <leo.dezan@gmail.com>
2023-01-03 21:33:17 -0300

    conexão DB

--- cadastro-clientes.php
+++ cadastro-clientes.php
@@ -1,16 +1,32 @@
+<!-- BACK END DA PAGINA cadastro-clientes.html -->
+
+
 <?php
-if ($_SERVER['REQUEST_METHOD'] == "POST"){
-    $data_cadastro = $_POST['data_cadastro'];
+include_once "config.php";
+
+?>
+
+<?php
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
     $tipo = $_POST['tipo'];
     $cnpj = $_POST['cnpj'];
-    $razao_social = $_POST['razao-social'];
+    $razao_social = $_POST['razao_social'];
     $ie = $_POST['ie'];
     $endereco = $_POST['endereco'];
     $bairro = $_POST['bairro'];
     $cidade = $_POST['cidade'];
     $email = $_POST['email'];
-    $fone_compras = $_POST['fone-compras'];
+    $fone_compras = $_POST['fone_compras'];
     $comprador = $_POST['comprador'];
+
+    $sql = "INSERT INTO tb_clientes(tipo, cnpj, razao_social, ie, endereco, bairro, cidade, email,fone_compras,comprador)VALUES('$tipo','$cnpj','$razao_social','$ie','$endereco','$bairro','$cidade','$email','$fone_compras','$comprador')";
+
+    if(mysqli_query($conexao, $sql)){
+        header("Location:html/cadastro-clientes.html");
+    }else{
+        echo "deu erro" . $sql . "<br/>" . mysqli_error($conexao);
+   }
+    mysqli_close($conexao);
 
 }
 
---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+$servidor = "localhost";
+$dbusuario = "root";
+$dbsenha = "";
+$dbnome = "db_sistemy_brastem";
+$conexao = mysqli_connect($servidor, $dbusuario, $dbsenha, $dbnome);
+?>
