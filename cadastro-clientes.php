f7e42f54a06a03a32f38266bdd6e60a3ef8ef569
Leonardo Dezan <leo.dezan@gmail.com>
2023-01-09 19:53:30 -0300

    Update cadastro-clientes.php

--- cadastro-clientes.php
+++ cadastro-clientes.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Pesquisar</title>
+    <title>Cadastro de Clientes</title>
 </head>
 </html>
 
@@ -37,22 +37,6 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
         echo "deu erro" . $sql . "<br/>" . mysqli_error($conexao);
    }
     mysqli_close($conexao);
-
-}
-
-// PESQUISA DE INFORMAÇÕES NA TABELA DE CLIENTES DO BANCO DE DAODS
-if (isset($_GET['pesquisar'])){
-    $pesquisar = $_GET['pesquisar'];
-    $resultado_pesquisar = "SELECT * FROM tb_clientes WHERE razao_social like '%$pesquisar%' or cnpj like '%$pesquisar%'";
-    $resultado = mysqli_query($conexao, $resultado_pesquisar);
-    while ($linha = mysqli_fetch_array($resultado)){
-        echo $search_id = "-" . $linha['id'];
-        echo $search_razao =  "-" . $linha['razao_social'];
-        echo $search_cnpj =  "-" . $linha['cnpj'];
-        echo $search_cnpj = "<a href='#'". $linha['id'] . '>Deletar</a>';
-        echo "<br/>";
-    }
-
 
 }
 
